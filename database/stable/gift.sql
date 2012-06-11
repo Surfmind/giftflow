35e28ea157c55343fa6c50a924165e42839d8109
Hans Schoenburg <hschoenburg@gmail.com>
2012-06-11 08:49:23 -0400

    Thank you - database changes, plus a small css fix.
    To get the thankyou system working in the database, you need the links table and the thankyou template inserted into the terms table. The links table stores the hashes used in the Accept/Decline email sent to the end user. Clicking on those urls performs an action, thus the hashes are needed to prevent malicious thanking.

--- database/stable/gift.sql
+++ database/stable/gift.sql
@@ -608,6 +608,23 @@ DEFAULT CHARACTER SET = utf8;
 
 CREATE INDEX `user_id` ON `user_settings` (`user_id` ASC) ;
 
+-- ----------------------------------------------------
+-- Table `links`
+-- ----------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS `links` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `sender_id` int(10) NOT NULL,
+  `recipient_id` int(11) NOT NULL,
+  `type` enum('thankyou') NOT NULL,
+  `transaction_id` int(100) NOT NULL,
+  `code` varchar(200) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) 
+ENGINE=InnoDB  
+DEFAULT CHARSET=utf8;
+
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
